70b87c8c3419d012cd6d10d96c7dd95f9b47439b
Patrick Schuur <patrickschuurdev@gmail.com>
2025-10-03 01:10:52 +0200

    Command pool and buffer

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -39,6 +39,8 @@ void Game::initVulkan() {
     createSwapChain();
     createImageViews();
     createGraphicsPipeline();
+    createCommandPool();
+    createCommandBuffer();
 }
 
 void Game::createInstance() {
@@ -173,7 +175,6 @@ void Game::createLogicalDevice() {
     // find the index of the first queue family that supports graphics
     std::vector<vk::QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
 
-    uint32_t queueIndex = ~0;
     for (uint32_t qfpIndex = 0; qfpIndex < queueFamilyProperties.size(); ++qfpIndex) {
         if (queueFamilyProperties[qfpIndex].queueFlags & vk::QueueFlagBits::eGraphics &&
             physicalDevice.getSurfaceSupportKHR(qfpIndex, *surface)) {
@@ -214,10 +215,10 @@ void Game::createLogicalDevice() {
 }
 
 void Game::createSwapChain() {
-    auto surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR(*surface);
+    const auto surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR(*surface);
     swapChainExtent = chooseSwapExtent(surfaceCapabilities);
     swapChainSurfaceFormat = chooseSwapSurfaceFormat(physicalDevice.getSurfaceFormatsKHR(*surface));
-    vk::SwapchainCreateInfoKHR swapChainCreateInfo{
+    const vk::SwapchainCreateInfoKHR swapChainCreateInfo{
         .surface = *surface,
         .minImageCount = chooseSwapMinImageCount(surfaceCapabilities),
         .imageFormat = swapChainSurfaceFormat.format,
@@ -243,7 +244,7 @@ void Game::createImageViews() {
         .viewType = vk::ImageViewType::e2D, .format = swapChainSurfaceFormat.format,
         .subresourceRange = {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1}
     };
-    for (auto image: swapChainImages) {
+    for (const auto image: swapChainImages) {
         imageViewCreateInfo.image = image;
         swapChainImageViews.emplace_back(device, imageViewCreateInfo);
     }
@@ -311,6 +312,104 @@ void Game::createGraphicsPipeline() {
     };
 
     graphicsPipeline = vk::raii::Pipeline(device, nullptr, pipelineInfo);
+}
+
+void Game::createCommandPool() {
+    const vk::CommandPoolCreateInfo poolInfo{
+        .flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
+        .queueFamilyIndex = queueIndex
+    };
+    commandPool = vk::raii::CommandPool(device, poolInfo);
+}
+
+void Game::createCommandBuffer() {
+    const vk::CommandBufferAllocateInfo allocInfo{
+        .commandPool = commandPool, .level = vk::CommandBufferLevel::ePrimary,
+        .commandBufferCount = 1
+    };
+    commandBuffer = std::move(vk::raii::CommandBuffers(device, allocInfo).front());
+}
+
+void Game::recordCommandBuffer(const uint32_t imageIndex) {
+    commandBuffer.begin({});
+    // Before starting rendering, transition the swapchain image to COLOR_ATTACHMENT_OPTIMAL
+    transition_image_layout(
+        imageIndex,
+        vk::ImageLayout::eUndefined,
+        vk::ImageLayout::eColorAttachmentOptimal,
+        {}, // srcAccessMask (no need to wait for previous operations)
+        vk::AccessFlagBits2::eColorAttachmentWrite, // dstAccessMask
+        vk::PipelineStageFlagBits2::eTopOfPipe, // srcStage
+        vk::PipelineStageFlagBits2::eColorAttachmentOutput // dstStage
+    );
+    constexpr vk::ClearValue clearColor = vk::ClearColorValue(0.0f, 0.0f, 0.0f, 1.0f);
+    vk::RenderingAttachmentInfo attachmentInfo = {
+        .imageView = swapChainImageViews[imageIndex],
+        .imageLayout = vk::ImageLayout::eColorAttachmentOptimal,
+        .loadOp = vk::AttachmentLoadOp::eClear,
+        .storeOp = vk::AttachmentStoreOp::eStore,
+        .clearValue = clearColor
+    };
+    const vk::RenderingInfo renderingInfo = {
+        .renderArea = {.offset = {0, 0}, .extent = swapChainExtent},
+        .layerCount = 1,
+        .colorAttachmentCount = 1,
+        .pColorAttachments = &attachmentInfo
+    };
+
+    commandBuffer.beginRendering(renderingInfo);
+    commandBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, *graphicsPipeline);
+    commandBuffer.setViewport(0, vk::Viewport(0.0f, 0.0f, static_cast<float>(swapChainExtent.width),
+                                              static_cast<float>(swapChainExtent.height), 0.0f, 1.0f));
+    commandBuffer.setScissor(0, vk::Rect2D(vk::Offset2D(0, 0), swapChainExtent));
+    commandBuffer.draw(3, 1, 0, 0);
+    commandBuffer.endRendering();
+    // After rendering, transition the swapchain image to PRESENT_SRC
+    transition_image_layout(
+        imageIndex,
+        vk::ImageLayout::eColorAttachmentOptimal,
+        vk::ImageLayout::ePresentSrcKHR,
+        vk::AccessFlagBits2::eColorAttachmentWrite, // srcAccessMask
+        {}, // dstAccessMask
+        vk::PipelineStageFlagBits2::eColorAttachmentOutput, // srcStage
+        vk::PipelineStageFlagBits2::eBottomOfPipe // dstStage
+    );
+    commandBuffer.end();
+}
+
+void Game::transition_image_layout(
+    const uint32_t currentFrame,
+    const vk::ImageLayout old_layout,
+    const vk::ImageLayout new_layout,
+    const vk::AccessFlags2 src_access_mask,
+    const vk::AccessFlags2 dst_access_mask,
+    const vk::PipelineStageFlags2 src_stage_mask,
+    const vk::PipelineStageFlags2 dst_stage_mask
+) {
+    vk::ImageMemoryBarrier2 barrier = {
+        .srcStageMask = src_stage_mask,
+        .srcAccessMask = src_access_mask,
+        .dstStageMask = dst_stage_mask,
+        .dstAccessMask = dst_access_mask,
+        .oldLayout = old_layout,
+        .newLayout = new_layout,
+        .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+        .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+        .image = swapChainImages[currentFrame],
+        .subresourceRange = {
+            .aspectMask = vk::ImageAspectFlagBits::eColor,
+            .baseMipLevel = 0,
+            .levelCount = 1,
+            .baseArrayLayer = 0,
+            .layerCount = 1
+        }
+    };
+    const vk::DependencyInfo dependency_info = {
+        .dependencyFlags = {},
+        .imageMemoryBarrierCount = 1,
+        .pImageMemoryBarriers = &barrier
+    };
+    commandBuffer.pipelineBarrier2(dependency_info);
 }
 
 vk::raii::ShaderModule Game::createShaderModule(const std::vector<char> &code) const {
--- src/game/game.hpp
+++ src/game/game.hpp
@@ -8,63 +8,73 @@ class Game {
 public: // Properties
 
 private: // Member Variables
-	std::unique_ptr<Window> window;
+    std::unique_ptr<Window> window;
 
-	vk::raii::Context context;
-	vk::raii::Instance instance = nullptr;
-	vk::raii::PhysicalDevice physicalDevice = nullptr;
-	vk::raii::Device device = nullptr;
-	vk::raii::Queue graphicsQueue = nullptr;
-	vk::raii::SwapchainKHR           swapChain      = nullptr;
-	std::vector<vk::Image>           swapChainImages;
-	vk::SurfaceFormatKHR             swapChainSurfaceFormat;
-	vk::Extent2D                     swapChainExtent;
-	std::vector<vk::raii::ImageView> swapChainImageViews;
+    vk::raii::Context context;
+    vk::raii::Instance instance = nullptr;
+    vk::raii::PhysicalDevice physicalDevice = nullptr;
+    vk::raii::Device device = nullptr;
+    uint32_t queueIndex = ~0;
+    vk::raii::Queue graphicsQueue = nullptr;
+    vk::raii::SwapchainKHR swapChain = nullptr;
+    std::vector<vk::Image> swapChainImages;
+    vk::SurfaceFormatKHR swapChainSurfaceFormat;
+    vk::Extent2D swapChainExtent;
+    std::vector<vk::raii::ImageView> swapChainImageViews;
 
-	vk::raii::PipelineLayout pipelineLayout = nullptr;
-	vk::raii::Pipeline graphicsPipeline = nullptr;
+    vk::raii::PipelineLayout pipelineLayout = nullptr;
+    vk::raii::Pipeline graphicsPipeline = nullptr;
+    vk::raii::CommandPool commandPool = nullptr;
+    vk::raii::CommandBuffer commandBuffer = nullptr;
 
-	std::vector<const char*> requiredLayers{};
-	std::vector<const char*> extensions{};
+    std::vector<const char *> requiredLayers{};
+    std::vector<const char *> extensions{};
 
-	vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
+    vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
 
-	vk::raii::SurfaceKHR surface = nullptr;
-	vk::raii::Queue presentQueue = nullptr;
+    vk::raii::SurfaceKHR surface = nullptr;
+    vk::raii::Queue presentQueue = nullptr;
 
-	std::vector<const char*> deviceExtensions = {
-		vk::KHRSwapchainExtensionName,
-		vk::KHRSpirv14ExtensionName,
-		vk::KHRSynchronization2ExtensionName,
-		vk::KHRCreateRenderpass2ExtensionName
-	};
-
+    std::vector<const char *> deviceExtensions = {
+        vk::KHRSwapchainExtensionName,
+        vk::KHRSpirv14ExtensionName,
+        vk::KHRSynchronization2ExtensionName,
+        vk::KHRCreateRenderpass2ExtensionName
+    };
 
 public: // Public Functions
-	Game();
-	~Game();
+    Game();
+    ~Game();
 
-	Game(const Game&) = delete;
-	Game(Game&&) = delete;
-	Game& operator=(const Game&) = delete;
+    Game(const Game &) = delete;
+    Game(Game &&) = delete;
+    Game &operator=(const Game &) = delete;
 
-	void start();
+    void start();
 
 private: // Private Methods
-	void initVulkan();
-	void createInstance();
-	void setupDebugMessenger();
-	void createSurface();
-	void pickPhysicalDevice();
-	void createLogicalDevice();
-	void createSwapChain();
-	void createImageViews();
-	void createGraphicsPipeline();
-	[[nodiscard]] vk::raii::ShaderModule createShaderModule(const std::vector<char> &code) const;
+    void initVulkan();
+    void createInstance();
+    void setupDebugMessenger();
+    void createSurface();
+    void pickPhysicalDevice();
+    void createLogicalDevice();
+    void createSwapChain();
+    void createImageViews();
+    void createGraphicsPipeline();
+    void createCommandPool();
+    void createCommandBuffer();
 
-	uint32_t chooseSwapMinImageCount(vk::SurfaceCapabilitiesKHR const & surfaceCapabilities);
-	vk::SurfaceFormatKHR chooseSwapSurfaceFormat(std::vector<vk::SurfaceFormatKHR> const & availableFormats);
-	vk::PresentModeKHR chooseSwapPresentMode(const std::vector<vk::PresentModeKHR>& availablePresentModes);
-	[[nodiscard]] vk::Extent2D chooseSwapExtent(const vk::SurfaceCapabilitiesKHR &capabilities) const;
-	std::vector<char> readFile(const std::string &filename);
+    void recordCommandBuffer(uint32_t imageIndex);
+    void transition_image_layout(uint32_t currentFrame, vk::ImageLayout old_layout, vk::ImageLayout new_layout,
+                                 vk::AccessFlags2 src_access_mask, vk::AccessFlags2 dst_access_mask,
+                                 vk::PipelineStageFlags2 src_stage_mask, vk::PipelineStageFlags2 dst_stage_mask);
+
+
+    [[nodiscard]] vk::raii::ShaderModule createShaderModule(const std::vector<char> &code) const;
+    uint32_t chooseSwapMinImageCount(vk::SurfaceCapabilitiesKHR const &surfaceCapabilities);
+    vk::SurfaceFormatKHR chooseSwapSurfaceFormat(std::vector<vk::SurfaceFormatKHR> const &availableFormats);
+    vk::PresentModeKHR chooseSwapPresentMode(const std::vector<vk::PresentModeKHR> &availablePresentModes);
+    [[nodiscard]] vk::Extent2D chooseSwapExtent(const vk::SurfaceCapabilitiesKHR &capabilities) const;
+    std::vector<char> readFile(const std::string &filename);
 };
